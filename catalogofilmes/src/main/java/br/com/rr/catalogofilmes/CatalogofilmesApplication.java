75f139b798cf0931f8f520b3ef4e527e737ad94b
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2024-05-18 19:02:55 -0300

    Atualizando classes no Spring

--- catalogofilmes/src/main/java/br/com/rr/catalogofilmes/CatalogofilmesApplication.java
+++ catalogofilmes/src/main/java/br/com/rr/catalogofilmes/CatalogofilmesApplication.java
@@ -1,6 +1,8 @@
 package br.com.rr.catalogofilmes;
 
+import br.com.rr.catalogofilmes.model.DadosSerie;
 import br.com.rr.catalogofilmes.service.ConsumoApi;
+import br.com.rr.catalogofilmes.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +17,11 @@ public class CatalogofilmesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends+&Season=1&apikey=6ce6e40e");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=6ce6e40e");
 		System.out.println(json);
 
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
